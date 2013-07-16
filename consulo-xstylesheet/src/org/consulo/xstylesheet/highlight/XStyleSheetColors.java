61b60a6d50632d500c363ac20a9ed8e18a229c50
VISTALL <vistall.valeriy@gmail.com>
2013-07-16 18:17:30 +0300

    highlight scheme update

--- consulo-xstylesheet/src/org/consulo/xstylesheet/highlight/XStyleSheetColors.java
+++ consulo-xstylesheet/src/org/consulo/xstylesheet/highlight/XStyleSheetColors.java
@@ -14,17 +14,17 @@ public interface XStyleSheetColors {
 
 	TextAttributesKey BLOCK_COMMENT = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_BLOCK_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT);
 
-	TextAttributesKey KEYWORD = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
+	TextAttributesKey KEYWORD = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_KEYWORD", DefaultLanguageHighlighterColors.STRING);
 
 	TextAttributesKey SELECTOR_ELEMENT_NAME = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_SELECTOR_ELEMENT_NAME", DefaultLanguageHighlighterColors.KEYWORD);
 
-	TextAttributesKey SELECTOR_CLASS_NAME = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_SELECTOR_CLASS_NAME", DefaultLanguageHighlighterColors.INSTANCE_FIELD);
+	TextAttributesKey SELECTOR_CLASS_NAME = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_SELECTOR_CLASS_NAME", DefaultLanguageHighlighterColors.KEYWORD);
 
-	TextAttributesKey SELECTOR_ID_NAME = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_SELECTOR_ID_NAME", DefaultLanguageHighlighterColors.METADATA);
+	TextAttributesKey SELECTOR_ID_NAME = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_SELECTOR_ID_NAME", DefaultLanguageHighlighterColors.KEYWORD);
 
 	TextAttributesKey ATTRIBUTE_NAME = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_ATTRIBUTE_NAME", DefaultLanguageHighlighterColors.MARKUP_ATTRIBUTE);
 
-	TextAttributesKey PROPERTY_NAME = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_PROPERTY_NAME", DefaultLanguageHighlighterColors.IDENTIFIER);
+	TextAttributesKey PROPERTY_NAME = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_PROPERTY_NAME", DefaultLanguageHighlighterColors.MARKUP_ATTRIBUTE);
 
 	TextAttributesKey PSEUDO_NAME = TextAttributesKey.createTextAttributesKey("XSTYLESHEET_PSEUDO_NAME", DefaultLanguageHighlighterColors.MARKUP_ATTRIBUTE);
 }
