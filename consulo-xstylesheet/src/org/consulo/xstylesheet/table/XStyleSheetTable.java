2814d4b5d18d778e05443cf2e722f161c3dbff95
VISTALL <vistall.valeriy@gmail.com>
2013-12-23 12:20:04 +0200

    copyright. updated project

--- consulo-xstylesheet/src/org/consulo/xstylesheet/table/XStyleSheetTable.java
+++ consulo-xstylesheet/src/org/consulo/xstylesheet/table/XStyleSheetTable.java
@@ -14,23 +14,12 @@
  * limitations under the License.
  */
 
-package org.consulo.xstylesheet.definition;
-
-import org.consulo.annotations.Immutable;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
+package org.consulo.xstylesheet.table;
 
 /**
  * @author VISTALL
- * @since 03.07.13.
+ * @since 10.12.13.
  */
-public interface XStyleSheetTable {
-  @Nullable
-  XStyleSheetProperty findProperty(@NotNull String propertyName);
-
-  @NotNull
-  @Immutable
-  List<XStyleSheetProperty> getProperties();
+public class XStyleSheetTable
+{
 }
