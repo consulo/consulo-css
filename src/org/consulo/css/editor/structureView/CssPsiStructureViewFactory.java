e71a845df88d95e93af11bf9410b9a880d725947
VISTALL <vistall.valeriy@gmail.com>
2014-02-09 01:27:55 +0200

    compilation fixed

--- src/org/consulo/css/editor/structureView/CssPsiStructureViewFactory.java
+++ src/org/consulo/css/editor/structureView/CssPsiStructureViewFactory.java
@@ -16,30 +16,35 @@
 
 package org.consulo.css.editor.structureView;
 
+import org.consulo.css.lang.psi.CssFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.StructureViewModelBase;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
-import org.consulo.css.lang.psi.CssFile;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * @author VISTALL
  * @since 08.07.13.
  */
-public class CssPsiStructureViewFactory implements PsiStructureViewFactory {
-  @Nullable
-  @Override
-  public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
-    return new TreeBasedStructureViewBuilder() {
-      @NotNull
-      @Override
-      public StructureViewModel createStructureViewModel() {
-        return new StructureViewModelBase(psiFile, new CssStructureViewTreeElement((CssFile) psiFile));
-      }
-    };
-  }
+public class CssPsiStructureViewFactory implements PsiStructureViewFactory
+{
+	@Nullable
+	@Override
+	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile)
+	{
+		return new TreeBasedStructureViewBuilder()
+		{
+			@NotNull
+			@Override
+			public StructureViewModel createStructureViewModel(Editor editor)
+			{
+				return new StructureViewModelBase(psiFile, new CssStructureViewTreeElement((CssFile) psiFile));
+			}
+		};
+	}
 }
