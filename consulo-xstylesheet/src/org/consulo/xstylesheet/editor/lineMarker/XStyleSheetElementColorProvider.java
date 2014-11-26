53df4d5e72445857550cd23fb160c4538e3da41f
VISTALL <vistall.valeriy@gmail.com>
2014-11-26 20:17:11 +0300

    fixed showing color markers for css

--- consulo-xstylesheet/src/org/consulo/xstylesheet/editor/lineMarker/XStyleSheetElementColorProvider.java
+++ consulo-xstylesheet/src/org/consulo/xstylesheet/editor/lineMarker/XStyleSheetElementColorProvider.java
@@ -28,11 +28,14 @@ import com.intellij.psi.PsiElement;
  * @author VISTALL
  * @since 03.07.13.
  */
-public class XStyleSheetElementColorProvider implements ElementColorProvider {
-    @Nullable
-    @Override
-    public Color getColorFrom(@NotNull PsiElement psiElement) {
-        if(psiElement instanceof PsiXStyleSheetPropertyValuePart) {
+public class XStyleSheetElementColorProvider implements ElementColorProvider
+{
+	@Nullable
+	@Override
+	public Color getColorFrom(@NotNull PsiElement psiElement)
+	{
+		if(psiElement instanceof PsiXStyleSheetPropertyValuePart)
+		{
 			Object value = null;
 			try
 			{
@@ -43,15 +46,17 @@ public class XStyleSheetElementColorProvider implements ElementColorProvider {
 				System.out.println(psiElement.getText());
 				e.printStackTrace();
 			}
-			if(value instanceof Color) {
-            return (Color) value;
-          }
-        }
-        return null;
-    }
+			if(value instanceof Color)
+			{
+				return (Color) value;
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public void setColorTo(@NotNull PsiElement psiElement, @NotNull Color color) {
-        //TODO [VISTALL] !
-    }
+	@Override
+	public void setColorTo(@NotNull PsiElement psiElement, @NotNull Color color)
+	{
+		//TODO [VISTALL] !
+	}
 }
